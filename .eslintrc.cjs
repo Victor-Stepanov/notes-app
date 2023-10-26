fe05d60c88e4e40feb836c322ad40e504d18c548
Victor Stepanov <vix522430@bk.ru>
2023-10-26 18:54:36 +0300

    feat: добавил rtk, линтеры, husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,42 +2,42 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:prettier/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       typescript: {},
     },
   },
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh", "prettier", "import"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'prettier', 'import'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
+    'react-refresh/only-export-components': [
+      'warn',
       { allowConstantExport: true },
     ],
-    "prettier/prettier": 2,
-    "import/order": [
+    'prettier/prettier': 2,
+    'import/order': [
       2,
       {
         groups: [
-          "external",
-          "builtin",
-          "index",
-          "sibling",
-          "parent",
-          "internal",
-          "type",
+          'external',
+          'builtin',
+          'index',
+          'sibling',
+          'parent',
+          'internal',
+          'type',
         ],
         alphabetize: {
-          order: "asc",
+          order: 'asc',
           caseInsensitive: true,
         },
-        "newlines-between": "always-and-inside-groups",
+        'newlines-between': 'always-and-inside-groups',
       },
     ],
   },
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
-import { App } from "./App.tsx";
-import "./index.css";
-import { store } from "./store/store.ts";
+import { App } from './App.tsx';
+import './index.css';
+import { store } from './store/store.ts';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
